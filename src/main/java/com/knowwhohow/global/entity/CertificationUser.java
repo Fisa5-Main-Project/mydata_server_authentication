accb5aec29c8555bd3ffa5db92d207197205794c
woni-jo <woni0517@naver.com>
2025-12-01 14:36:46 +0900

    [feat] jpa가 encry및 decrpyt 하도록 설정

--- src/main/java/com/knowwhohow/global/entity/CertificationUser.java
+++ src/main/java/com/knowwhohow/global/entity/CertificationUser.java
@@ -1,5 +1,8 @@
 package com.knowwhohow.global.entity;
 
+//import com.knowwhohow.global.config.EncryptConverter;
+import com.knowwhohow.global.config.EncryptConverter;
+import com.knowwhohow.global.config.LocalDateEncryptConverter;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -16,18 +19,21 @@ public class CertificationUser {
     private Long id;
 
     @Column(nullable = false)
+    @Convert(converter = EncryptConverter.class)
     private String name; // 이름
 
     @Column(nullable = false)
     private String gender; // 성별 (M/F)
 
     @Column(nullable = false)
+    @Convert(converter = LocalDateEncryptConverter.class)
     private LocalDate birthDate; // 생년월일
 
     @Column(nullable = false)
     private String carrier; // 통신사
 
     @Column(nullable = false)
+//    @Convert(converter = EncryptConverter.class)
     private String phoneNumber; // 전화번호
 
     @Column(nullable = false, unique = true)
