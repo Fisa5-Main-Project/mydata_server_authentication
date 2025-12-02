7ae3ac2bf3f485621d04a670e94f311ae1be646a
woni-jo <woni0517@naver.com>
2025-12-02 18:16:02 +0900

    [feat] DB 검색을 위한 hash 설정

--- src/main/java/com/knowwhohow/service/CertAuthService.java
+++ src/main/java/com/knowwhohow/service/CertAuthService.java
@@ -2,6 +2,7 @@ package com.knowwhohow.service;
 
 import com.knowwhohow.dto.FetchCertRequestDTO;
 import com.knowwhohow.dto.FetchCertResponseDTO;
+import com.knowwhohow.global.config.HashUtil;
 import com.knowwhohow.global.exception.CustomException;
 import com.knowwhohow.global.exception.ErrorCode;
 import com.knowwhohow.repository.CertificationUserRepository;
@@ -12,9 +13,13 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CertAuthService {
     private final CertificationUserRepository certificationUserRepository;
+    private final HashUtil hashUtil;
 
     public FetchCertResponseDTO fetchAndSaveCertUser(FetchCertRequestDTO request) {
-        String ci = certificationUserRepository.findCiByNameAndPhoneNumberAndCarrier(request.name(), request.phone(), request.carrier());
+        String nameHash = hashUtil.generateHash(request.name());
+        String phoneHash = hashUtil.generateHash(request.phone());
+
+        String ci = certificationUserRepository.findCiByNameAndPhoneNumberAndCarrier(nameHash, phoneHash, request.carrier());
 
         if(ci == null) {
             throw new CustomException(ErrorCode.NOT_USER);
